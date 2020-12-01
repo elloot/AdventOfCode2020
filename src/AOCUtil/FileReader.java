93fe15036a6c8664053bc68ccda3d7d7f60dfe20
elloot <elliot.duchek@gmail.com>
2020-12-01 13:42:33 +0100

    add a file reader

---
+++ src/AOCUtil/FileReader.java
@@ -0,0 +1,26 @@
+package AOCUtil;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileReader {
+    public static String readFile(String fileName) {
+        Scanner s = null;
+
+        try {
+            s = new Scanner(new File(fileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        StringBuilder input = new StringBuilder();
+
+        while (s.hasNextLine()) {
+            input.append(s.nextLine()).append("\n");
+        }
+
+        return input.toString();
+    }
+}
+
