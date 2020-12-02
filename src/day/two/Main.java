626e7cc1145a075ea6a9f15c0684a141b57bb564
elloot <elliot.duchek@gmail.com>
2020-12-02 20:02:14 +0100

    solve day 2 part 2, move first solution to method

--- src/day/two/Main.java
+++ src/day/two/Main.java
@@ -1,4 +1,5 @@
 package day.two;
+
 import AOCUtil.*;
 
 public class Main {
@@ -6,6 +7,30 @@ public class Main {
         String inputString = FileReader.readFile("two/input.txt");
         String[] input = inputString.split("\n");
 
+        //numPwdOldPolicy(input);
+        numPwdNewPolicy(input);
+    }
+
+    public static void numPwdNewPolicy(String[] input) {
+        int validPasswords = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            String current = input[i];
+
+            int firstIndexLetter = Integer.parseInt(current.substring(0, current.indexOf("-"))) - 1;
+            int secondIndexLetter = Integer.parseInt(current.substring(current.indexOf("-") + 1, current.indexOf(" "))) - 1;
+            char currentLetter = current.charAt(current.indexOf(" ") + 1);
+            String currentPassword = current.substring(current.indexOf(":") + 2);
+
+
+            if (currentPassword.charAt(firstIndexLetter) != currentPassword.charAt(secondIndexLetter) && (currentPassword.charAt(firstIndexLetter) == currentLetter || currentPassword.charAt(secondIndexLetter) == currentLetter)) {
+                validPasswords++;
+            }
+        }
+        System.out.println(validPasswords);
+    }
+
+    public static void numPwdOldPolicy(String[] input) {
         int validPasswords = 0;
 
         for (int i = 0; i < input.length; i++) {
