ac2d9c6478c1ebae0e9a8f1d04de57263058e69f
elloot <elliot.duchek@gmail.com>
2020-12-04 08:28:39 +0100

    rename inputarray to passports

--- src/day/four/Main.java
+++ src/day/four/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         String inputString = FileReader.readFile("four/input.txt");
-        ArrayList<String> inputArray = new ArrayList<>();
+        ArrayList<String> passports = new ArrayList<>();
 
         //char previousChar = inputString.charAt(0);
         char currentChar;
@@ -19,13 +19,13 @@ public class Main {
             if (i + 1 < (inputString.length() - 1)) nextChar = inputString.charAt(i + 1);
             paragraphEndingIndex = i;
             if (currentChar == '\n' && nextChar == '\n') {
-                inputArray.add(inputString.substring(paragraphStartingIndex, paragraphEndingIndex));
+                passports.add(inputString.substring(paragraphStartingIndex, paragraphEndingIndex));
                 if (i + 2 < (inputString.length() - 1)) paragraphStartingIndex = i + 2;
             }
         }
 
-        for (int i = 0; i < inputArray.size(); i++) {
-            System.out.println(inputArray.get(i));
+        for (int i = 0; i < passports.size(); i++) {
+            System.out.println(passports.get(i));
             System.out.println();
         }
     }
