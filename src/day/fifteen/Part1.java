36c1535a7693fe1a5e1777dd71e2eeec2413189d
elloot <elliot.duchek@gmail.com>
2020-12-16 16:50:37 +0100

    solve day 15 part 1

---
+++ src/day/fifteen/Part1.java
@@ -0,0 +1,64 @@
+package day.fifteen;
+
+import AOCUtil.Timer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Part1 {
+    public static void main(String[] args) {
+        Timer t = new Timer();
+        t.start();
+
+        int[] startingNumbers = {1, 0, 16, 5, 17, 4};
+        Map<Integer, Integer> numbers = new HashMap<>();
+        boolean spokenBefore = false;
+        int lastSpoken = -1;
+        for (int i = 1; i <= 2020; i++) {
+            if (i <= startingNumbers.length) {
+                lastSpoken = startingNumbers[i - 1];
+                numbers.put(lastSpoken, i);
+                spokenBefore = false;
+                continue;
+            }
+            if (!spokenBefore) {
+                if (numbers.containsKey(0)) {
+                    spokenBefore = true;
+                } else {
+                    numbers.put(0, i);
+                }
+                lastSpoken = 0;
+            } else {
+                int difference = i - 1 - numbers.get(lastSpoken);
+                if (numbers.containsKey(difference)) {
+                    spokenBefore = true;
+                } else {
+                    numbers.put(difference, i);
+                    spokenBefore = false;
+                }
+                numbers.replace(lastSpoken, i - 1);
+                lastSpoken = difference;
+            }
+        }
+
+        for (int key : numbers.keySet()) {
+            if (numbers.get(key) == 2020) {
+                System.out.println(key + ", took " + t.nextMillis() + " ms");
+                break;
+            }
+        }
+    }
+
+        private static boolean contains ( int num, ArrayList<Integer > numbers){
+            boolean contains = false;
+            for (int i = 0; i < numbers.size() - 1; i++) {
+                if (num == numbers.get(i)) {
+                    contains = true;
+                    break;
+                }
+            }
+            return contains;
+        }
+    }
